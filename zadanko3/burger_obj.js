c9e47d7079c48657a1ceddb4a202c42da85cdeb9
GabrielaWalczyk <41693642+lonelybutterfly@users.noreply.github.com>
2019-11-02 02:28:34 +0100

    Add files via upload

---
+++ zadanko3/burger_obj.js
@@ -0,0 +1,85 @@
+
+var knownIngradients = []; //Ta zmienna przechowywać będzie tabelę, w której znajdować się będą wszystkie składniki (obiekty), z których możemy zrobić burgera
+
+var menu = []; //Zmienna, która będzie przechowywać wszystkie burgery, które można u nas zamówić.
+
+  //Poniżej mamy konstruktor burgerów. Na jego podstawie możemy stworzyć nowe burgery
+  // Bread może być white lub dark
+  var Burger = function (name, typeOfBread, arrayOfIngredients, price){
+
+    // Poniżej przypisujemy wartości podane w w nawiasie przy tworzeniu burgera do odpowiednich kluczy
+    this.name = name;
+    if (typeOfBread=="white" || typeOfBread=="dark" ){
+    this.typeOfBread = typeOfBread;
+    } else {
+      console.log("Hey, we don't use that type of bread in our company. Your bread will be white.")
+      this.typeOfBread = "white";
+    }
+    this.arrayOfIngredients=[];
+    //zmienna pomocnicza - łatwiej będzie nam odnależć obiekty produkty po indeksie
+    this.indexOfIngradients=[];
+    //sprawdzamy czy istnieje taki produkt
+    for(var i = 0; i < arrayOfIngredients.length; i++){
+        for(var y=0; y < knownIngradients.length; y++){
+          if(arrayOfIngredients[i]==knownIngradients[y].name){
+            this.arrayOfIngredients.push(arrayOfIngredients[i]);
+            this.indexOfIngradients.push(y);
+          }
+        }
+      }
+
+    this.price = price;
+    // dodajemy burgera do lity burgerów
+    // Jako, że wrzucamy do tabeli cały obiekt, a nie tylko jego nawę (watość) nie musimy tworzyć meody zmieniającej nazwę
+    // Obiekt to referencyjny typ danych - co oznacza, że w momencie, kiedy dodamy go do tablicy - dodamy odwołanie do pewnego miejsca w pamięci
+    // komputera, w którym znajduje się nasz obiekt. Oznacza to, że w momencie kiedy zmienimy jakąkolwiek wartość w obiekcie zmieni się ona też w tablicy
+    menu.push(this);
+ };
+
+ // konstruktor składników
+ //Ostatni parametr dodaliśmy wraz z wartością domyślną, jeśli nie podamy żadnych wartości dla niego - zostanie mu przypisana wartość "no"
+ var Ingradient = function (name, color, description, allergens = "no"){
+       this.name = name;
+       this.color = color;
+       this.description = description;
+       this.allergens = allergens;
+       knownIngradients.push(this);
+ }
+//tworzenie nowych składników
+var cheese = new Ingradient("cheese", "yellow", "Our cheese is made from natural raw milk.", "milk");
+var ham = new Ingradient("ham", "#E69EAA", "The classic Easter ham, shaved from the bone. We cure and smoke it with the bone in, to add flavour and succulence to this ham.");
+var salad = new Ingradient("salad", "#589123", "Our salad is fresh and delicious.");
+
+
+//tworzenie nowych burgerów
+var cheeseBurger = new Burger('Cheese Burger', "white", ["cheese","salad"], 12);
+var hamBurger = new Burger('Hamburger', "dark", ["salad", "ham", "cheese"], 12);
+
+
+
+
+
+
+
+//miejsce na twoj kod
+
+
+
+
+
+
+//wyświetlanie burgerów z menu na stronie
+for( let item of menu){
+    let ingradients = "";
+    let description = "";
+  for(let x = 0; x<item.arrayOfIngredients.length; x++){
+    index = item.indexOfIngradients[x];
+    ingradients+="<div class = 'ingradient' id='"+ item.name+knownIngradients[index].name +"' style='background: "+knownIngradients[index].color +";'></div>";
+    description+="<div class = 'itemOnList' name='"+ item.name+knownIngradients[index].name +"'><div class = 'descItem'><div class='descName'>"+knownIngradients[index].name+"</div><div class='descText'>";
+    description+= knownIngradients[index].description+"</div><div class='alergens'>Allergens: "+ knownIngradients[index].allergens +"</div></div></div>"
+  }
+  let visualization = "<div class = 'burgerVis'><div class = 'topBread "+item.typeOfBread+"'></div>"+ingradients+"<div class = 'bottomBread "+item.typeOfBread+"'></div></div>";
+  let preparedCode = "<div class = 'product' id = "+item.name+"><div class='name'>"+item.name+"</div><div class='hamburgerBody'>"+visualization +"<div class='producsDescription'><div class='scrolled'>"+ description+"</div></div></div></div>";
+  document.getElementById("burgers").innerHTML+=preparedCode;
+
+}
---
+++ zadanko3/readmeTrescZadania.txt
@@ -0,0 +1,9 @@
+Zadania do wykonania: 
+
+Witamy w firmie "Burgery Tak Dobre Że Magda G. Turla Się Ze Szczęścia sp. Z O.O."
+Pomóż nam sprawić, by burgery były jeszcze pyszniejsze a Magda G. turlała się jeszcze szybciej.
+
+1. Dodaj 3 wymyślone przez siebie produkty, których użyjesz, aby stworzyć wyśmienitego Burgera.
+2. Dodaj do menu własnego burgera.
+
+! Swój kod wpisz najlepiej wmiejscu oznaczonym jako "//miejsce na twoj kod" !
