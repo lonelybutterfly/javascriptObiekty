cecad76eb466116a686b32a76a8ac4470cda4b13
GabrielaWalczyk <41693642+lonelybutterfly@users.noreply.github.com>
2019-11-14 21:34:29 +0100

    Add files via upload

---
+++ zadanko1/script.js
@@ -0,0 +1,13 @@
+document.getElementById("signInForm").addEventListener("click", function(event){
+  //zamiast true należy dodać warynek sprawdzający czy inputy są puste
+  // PODPOWIEDŹ złap inputy używając getElementById, następnie poszukaj wartości która mogłaby wskazać na to, co zostało wpisane do inputa.
+  // PODPOWIEDŹ jeśli input jest pusty to jego wartość == ""
+  if(false){
+  } else {
+    //tutaj musisz sprawić aby blok o id error się pokazał.
+    //PODPOWIEDŹ Złap element używając document.getElementById. Wejdź w wartość "style" za pomocą łańcucha i poszukaj tam wartości "display". Ustaw ją na block.
+
+    event.preventDefault(); //to sprawia, że forma nie zostaje przesłana - powstrzymuje ją przed wysłaniem
+
+  }
+});
