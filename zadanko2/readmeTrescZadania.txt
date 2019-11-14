a53882d91208dd86296a1ea3a7692150de7a16ad
GabrielaWalczyk <41693642+lonelybutterfly@users.noreply.github.com>
2019-11-14 23:04:50 +0100

    Add files via upload

---
+++ zadanko2/readmeTrescZadania.txt
@@ -0,0 +1,8 @@
+Zadanie 2
+
+W pliku script.js znajduje się obiekt złożony z innych obiektów. 
+Jest to lista użytkowników, którzy kupili w ostatnim czasie jakiś produkt w naszej firmie.
+Wypisz na stronie w odpowiednich blokach (np używając document.getElementById.innerHTML+=) nazwy użytkowników.
+
+Użyj iteracji - np. pętli for in
+
---
+++ zadanko2/script.js
@@ -0,0 +1,26 @@
+var users = {
+  Marylka323: {
+    isPaymentAccepted: "true",
+    price: 20
+  },
+  GrzegorzFajny: {
+    isPaymentAccepted: "true",
+    price: 12
+  },
+  Januszek: {
+    isPaymentAccepted: "false",
+    price: 34
+  },
+  KKobial: {
+    isPaymentAccepted: "true",
+    price: 23
+  },
+  Kalafior: {
+    isPaymentAccepted: "false",
+    price: 121
+  },
+  Mateuszek12: {
+    isPaymentAccepted: "true",
+    price: 90
+  }
+}
